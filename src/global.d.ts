e0f05054fa6b52e9730d9b6a120994f7c2fc6388
Shiromitsu <dev.siro@3tsu.dev>
2026-08-26 18:51:05 +0900

    feat: Add Pagefind

--- src/global.d.ts
+++ src/global.d.ts
@@ -7,3 +7,5 @@ declare module '@resvg/resvg-wasm/index_bg.wasm' {
   const content: string | ArrayBuffer | WebAssembly.Module;
   export default content;
 }
+
+declare module '@pagefind/component-ui/css';
